ac4174e2a4e29d867384d16a6df24e9ee2599285
fotini <f.batzoni@gmail.com>
2022-11-01 15:44:24 +0200

    Add ApplicationDbContext

---
+++ BookEShopWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookEShopWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookEShopWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //constructor 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
