eda46befbbb0bc3cb0a892c923e9786f938e86a3
fotini <f.batzoni@gmail.com>
2022-11-01 13:48:12 +0200

    Adding Data Annotations

--- BookEShopWeb/Models/Category.cs
+++ BookEShopWeb/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace BookEShopWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookEShopWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }
