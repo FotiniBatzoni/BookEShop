8554ca9cdf8a039e60e53436220abb2e49b10f06
fotini <f.batzoni@gmail.com>
2022-11-07 14:49:05 +0200

    Implementing Category Repository

---
+++ BookEShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookEShop.DataAccess.Repository.IRepository;
+using BookEShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookEShop.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+
+        IEnumerable<Category> IRepository<Category>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
