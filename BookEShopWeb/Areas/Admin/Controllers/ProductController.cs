64c682778086427cf94a0759309104d2c1846709
fotini <f.batzoni@gmail.com>
2022-11-10 20:51:44 +0200

    Updating Product

--- BookEShopWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookEShopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -79,15 +79,37 @@ namespace BookEShopWeb.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if (obj.Product.ImageUrl!=null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath)) 
+                        { 
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
                     }
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
-                _unitOfWork.Product.Add(obj.Product);
-                _unitOfWork.Product.Save();
-                TempData["success"] = "Product is created successfully";
+
+                if (obj.Product.Id == 0)
+                {
+
+                    _unitOfWork.Product.Add(obj.Product);
+                    _unitOfWork.Product.Save();
+                    TempData["success"] = "Product is created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                    _unitOfWork.Product.Save();
+                    TempData["success"] = "Product is updated successfully";
+                }
+
+
                 return RedirectToAction("Index");
             }
             return View(obj);
