9e5b7523ff17cce4ea2f3ba9539777056bccd8f3
fotini <f.batzoni@gmail.com>
2022-11-12 14:27:41 +0200

    Creating CompanyController

--- BookEShopWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BookEShopWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -2,7 +2,9 @@
 using BookEShop.DataAccess.Repository;
 using BookEShop.DataAccess.Repository.IRepository;
 using BookEShop.Models;
+using BookEShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace BookEShopWeb.Controllers
@@ -15,107 +17,90 @@ namespace BookEShopWeb.Controllers
         public CompanyController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+        
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Company> objCategoryList = _unitOfWork.Company.GetAll();
-            return View(objCategoryList);
-        }
-
-        //GET
-        public IActionResult Create()
-        {
             return View();
         }
 
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Company obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Company.Add(obj);
-                _unitOfWork.Company.Save();
-                TempData["success"] = "Company is created successfully";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-
-        }
-
-
         //GET
-        public IActionResult Edit(int? id)
+        public IActionResult Upsert(int? id)
         {
+            Company company = new();
 
             Debug.WriteLine(id);
             if (id== null || id==0)
             {
-                return NotFound();
+                //create product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(company);
             }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var companyFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(c => c.Id == id);
-            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
-
-            if(companyFromDbFirst == null)
+            else
             {
-                return NotFound();
+                //update Product
+                company = _unitOfWork.Company.GetFirstOrDefault(u=>u.Id==id);
+                return View(company);
             }
-            return View(companyFromDbFirst);
         }
 
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Company obj)
+        public IActionResult Upsert(Company obj)
         {
+            
             if (ModelState.IsValid)
-            {
-                _unitOfWork.Company.Update(obj);
-                _unitOfWork.Company.Save();
-                TempData["success"] = "Company is updated successfully";
+            { 
+                if (obj.Id == 0)
+               {
+
+                    _unitOfWork.Company.Add(obj);
+                    _unitOfWork.Company.Save();
+                    TempData["success"] = "Company is created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    _unitOfWork.Company.Save();
+                    TempData["success"] = "Company is updated successfully";
+                }
+
+
                 return RedirectToAction("Index");
             }
             return View(obj);
 
         }
 
-        //GET
-        public IActionResult Delete(int? id)
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
         {
-
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            // var companyFromDbFirst = _db.Company.Find(id);
-            var companyFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(c => c.Id == id);
-            //var companyFromDbFirst = _db.Company.SingleOrDefault(c => c.Id == id);
-
-            if (companyFromDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(companyFromDbFirst);
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
         }
 
         //POST
-        [HttpPost,ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
+        [HttpDelete]
+        public IActionResult Delete(int? id)
         {
             var obj = _unitOfWork.Company.GetFirstOrDefault(c => c.Id == id);
             if (obj == null)
             {
-                return NotFound();  
+                return Json(new { success = false, message = "Error while deleting" });
             }
 
-            _unitOfWork.Company.Remove(obj);
+     
+        _unitOfWork.Company.Remove(obj);
             _unitOfWork.Company.Save();
-            TempData["success"] = "Company is deleted successfully";
-            return RedirectToAction("Index");
+            return Json(new { success = true, message = "Delete Successful" });
         }
+        #endregion
     }
+
 }
