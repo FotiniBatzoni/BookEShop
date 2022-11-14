32a69059b578c94dc75d6d902ba413f753ed0479
fotini <f.batzoni@gmail.com>
2022-11-14 13:19:29 +0200

    Creating Shopping Card UI

---
+++ BookEShopWeb/Areas/Customer/Controllers/CardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookEShopWeb.Areas.Customer.Controllers
+{
+    public class CardController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
