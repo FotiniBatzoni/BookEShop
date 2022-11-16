f2c22e1cb54d2454e81ebcc5b8aae85e132893b5
fotini <f.batzoni@gmail.com>
2022-11-16 14:23:26 +0200

    Creating OrderVM and retrieve all orders

---
+++ BookEShopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using BookEShop.DataAccess.Repository;
+using BookEShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookEShopWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+
+    }
+}
