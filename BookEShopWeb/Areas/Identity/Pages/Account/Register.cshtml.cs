1be9ac96234bf24a426151b379e2dbeb4ac5baae
fotini <f.batzoni@gmail.com>
2022-11-19 12:31:46 +0200

    Hiding Roles From Registration

--- BookEShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookEShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -217,7 +217,17 @@ namespace BookEShopWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if(User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+              
+
+                        }
+                        else 
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                      
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
